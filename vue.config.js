381e927b002c9f4dd845fcbcb6e4b1a46205805f
Ting <hsiaoting1003@gmail.com>
2025-01-26 21:37:15 +0800

    調整為 createWebHashHistory 測試部屬

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/cafe-today/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cafe-today/'
+    : '/'
 });
