64c8dc2110fb357a77778ec5cc7057a7d6ce4df4
Ting <hsiaoting1003@gmail.com>
2025-01-29 17:53:50 +0800

    新增 footer 元件

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
   <div class="wrapper font-mono">
     <router-view />
   </div>
+  <FooterComponent></FooterComponent>
 </template>
 
 <script>
 import NavComponent from '@/components/NavComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 
 export default {
   name: 'App',
   components: {
     NavComponent,
+    FooterComponent,
   },
 };
 </script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer">
+    <p>Made by {{ nowYear }} Hsiao Ting Hong</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+  data() {
+    return {
+      nowYear: '',
+    };
+  },
+  mounted() {
+    this.getNowYear();
+  },
+  methods: {
+    getNowYear() {
+      const today = new Date();
+      this.nowYear = today.getFullYear();
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+@import "tailwindcss";
+
+.footer {
+  @apply h-[10vh] flex justify-center py-5
+  text-center text-xs text-stone-400;
+}
+</style>
