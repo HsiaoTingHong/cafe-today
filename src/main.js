be88b6c3c5902129c4a59b2d7ddef5337bf92a00
Ting <hsiaoting1003@gmail.com>
2025-01-29 13:35:35 +0800

    新增 Google font 字型 Amatic SC

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import './assets/all.css';
+import './assets/fonts.css';
 
 const app = createApp(App);
 app.use(VueAxios, axios);
