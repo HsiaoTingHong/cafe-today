fc36e4c40624da48c9f7665667ba3cafcb2f950e
Ting <hsiaoting1003@gmail.com>
2025-01-30 19:01:55 +0800

    安裝 vue3-loading-overlay

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Loading from 'vue3-loading-overlay';
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import App from './App.vue';
 import router from './router';
 import './assets/all.css';
@@ -9,4 +11,5 @@ import './assets/fonts.css';
 const app = createApp(App);
 app.use(VueAxios, axios);
 app.use(router);
+app.component('LoadingOverlay', Loading);
 app.mount('#app');
