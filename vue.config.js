3c557b9185a1844f5b6a6a3245523c59791d321c
Ting <hsiaoting1003@gmail.com>
2025-02-03 21:51:13 +0800

    關閉 Sourcemap，打包時就不會產生 .map 檔案，DevTools 也無法看到原始碼

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false, // 關閉 Sourcemap
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cafe-today/'
     : '/',
