68e521cedfa11f0b15cfe38e8a817095a77e3980
Ting <hsiaoting1003@gmail.com>
2025-03-29 22:32:50 +0800

    修正按鈕在 RWD 時按鈕寬度會變成填滿整行

--- src/views/CustomizeView.vue
+++ src/views/CustomizeView.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="min-h-[82vh]">
-    <p>你的咖啡小日子</p>
+    <!-- 你的咖啡小日子 -->
+
     <!-- 左側選單 -->
     <div class="">
-      <div class="">
-        <router-link to="/customize/customizeList" href="#" class="">
+      <div class="child-nav">
+        <router-link to="/customize/customizeList" href="#" class="button click-color-light">
           你的私藏咖啡巡禮
         </router-link>
-        <router-link to="/customize/customizeDestiny" href="#" class="">
+        <router-link to="/customize/customizeDestiny" href="#" class="button click-color-light">
           下一站要去哪?
         </router-link>
-        <router-link to="/customize/customizeEdit" href="#" class="">
+        <router-link to="/customize/customizeEdit" href="#" class="button click-color-light">
           編輯你的口袋名單
         </router-link>
       </div>
