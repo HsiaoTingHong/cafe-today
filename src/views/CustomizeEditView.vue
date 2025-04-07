a0f520bbef8ca94f28fec2426ee9d4727529e9a6
Ting <hsiaoting1003@gmail.com>
2025-04-07 14:13:42 +0800

    優化表單驗證邏輯

--- src/views/CustomizeEditView.vue
+++ src/views/CustomizeEditView.vue
@@ -11,9 +11,18 @@
           id="CustomizeCafeName"
           v-model="CustomizeCafe.name"
           placeholder="請輸入店家名稱(必填)"
-          class="input-text"
+          :class="[
+            'input-text',
+            errors.name ? 'error-input-text' : ''
+          ]"
         >
       </div>
+      <span
+        v-if="errors.name"
+        class="error-span"
+      >
+        {{ errors.name }}
+      </span>
 
       <div class="form-group input-label light-bold-text">
         <label for="CustomizeCafeCity">城市</label>
@@ -22,9 +31,18 @@
           id="CustomizeCafeCity"
           v-model="CustomizeCafe.city"
           placeholder="請輸入城市英文小寫(必填)"
-          class="input-text"
+          :class="[
+            'input-text',
+            errors.city ? 'error-input-text' : ''
+          ]"
         >
       </div>
+      <span
+        v-if="errors.city"
+        class="error-span"
+      >
+        {{ errors.city }}
+      </span>
 
       <div class="form-group input-label light-bold-text">
         <label for="CustomizeCafeAddress">地址</label>
@@ -33,9 +51,18 @@
           id="CustomizeCafeAddress"
           v-model="CustomizeCafe.address"
           placeholder="請輸入詳細中文地址(必填)"
-          class="input-text"
+          :class="[
+            'input-text',
+            errors.address ? 'error-input-text' : ''
+          ]"
         >
       </div>
+      <span
+        v-if="errors.address"
+        class="error-span"
+      >
+        {{ errors.address }}
+      </span>
 
       <div class="form-group input-label light-bold-text">
         <label for="CustomizeCafeHours">營業<br />時間</label>
@@ -43,10 +70,19 @@
           id="CustomizeCafeHours"
           v-model="CustomizeCafe.open_time"
           placeholder="請輸入營業時間，例如：週一至週五 09:00-21:00(必填)"
-          class="input-text"
+          :class="[
+            'input-text',
+            errors.open_time ? 'error-input-text' : ''
+          ]"
         >
         </textarea>
       </div>
+      <span
+        v-if="errors.open_time"
+        class="error-span"
+      >
+        {{ errors.open_time }}
+      </span>
 
       <div>
         <button type="submit" class="button click-color-light button-top">加入你的口袋名單</button>
@@ -75,6 +111,7 @@ export default {
     ModalBox,
   },
   setup() {
+    // 表單輸入資料
     const CustomizeCafe = reactive({
       id: uuidv4(),
       name: '',
@@ -92,14 +129,59 @@ export default {
       closeModal,
     } = useModal();
 
+    // 表單驗證
+    const errors = reactive({
+      name: '',
+      city: '',
+      address: '',
+      open_time: '',
+    });
+
+    const validateForm = () => {
+      let isValid = true;
+
+      // 清空錯誤訊息
+      Object.keys(errors).forEach((key) => {
+        errors[key] = '';
+      });
+
+      if (!CustomizeCafe.name.trim()) {
+        errors.name = '店家名稱為必填';
+        isValid = false;
+      }
+
+      if (!CustomizeCafe.city.trim()) {
+        errors.city = '城市為必填';
+        isValid = false;
+      } else if (!/^[a-z]+$/.test(CustomizeCafe.city)) {
+        errors.city = '城市請輸入英文小寫';
+        isValid = false;
+      }
+
+      if (!CustomizeCafe.address.trim()) {
+        errors.address = '地址為必填';
+        isValid = false;
+      } else if (!/^[\u4e00-\u9fa5\d\s]+$/.test(CustomizeCafe.address)) {
+        errors.address = '地址需為中文，可包含數字和空白';
+        isValid = false;
+      }
+
+      if (!CustomizeCafe.open_time.trim()) {
+        errors.open_time = '營業時間為必填';
+        isValid = false;
+      } else if (!/週.+?至週.+?\s\d{2}:\d{2}-\d{2}:\d{2}/.test(CustomizeCafe.open_time)) {
+        errors.open_time = '營業時間格式不符，範例：週一至週五 09:00-21:00';
+        isValid = false;
+      }
+
+      return isValid;
+    };
+
     // 送出表單
     const submitForm = () => {
       // 驗證表單
-      if (!CustomizeCafe.name
-          || !CustomizeCafe.city
-          || !CustomizeCafe.address
-          || !CustomizeCafe.open_time) {
-        openModal('請填寫所有必填欄位', 'error');
+      if (!validateForm()) {
+        openModal('請確認填寫的資料格式是否錯誤！', 'error');
         return;
       }
 
@@ -138,6 +220,7 @@ export default {
       closeModal,
       modalMessage,
       modalType,
+      errors,
     };
   },
 };
@@ -152,16 +235,28 @@ export default {
 }
 
 .form-group {
-  @apply flex text-nowrap text-left font-bold mb-4;
+  @apply flex text-nowrap text-left font-bold mt-4 mb-1;
 }
 
 .input-label {
   @apply flex justify-center items-center
-  text-nowrap mb-5;
+  text-nowrap;
 }
 
 .input-text {
   @apply min-w-[60vw] sm:min-w-[50vw] md:min-w-[40vw] lg:min-w-[30vw]
-  mx-4 p-2 rounded-lg border-1 border-stone-300;
+  ml-4 p-2 rounded-lg border-1 border-stone-300 text-sm;
+}
+
+.error-input-text {
+  @apply placeholder-red-300 border-1 border-red-300;
+}
+
+.error-span {
+  @apply text-red-500 text-sm mt-1 mb-4 ml-8;
+}
+
+.button-top {
+  @apply mt-8;
 }
 </style>
